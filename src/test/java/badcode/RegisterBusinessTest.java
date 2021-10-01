d3de6f59958f2e488b2e30a4e1c60d57c36a41cd
Arsom Mahatthanamongkol <arsom.ma@cdg.co.th>
2021-10-01 09:41:39 +0700

    Register Business Failure Test

---
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -0,0 +1,31 @@
+package badcode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RegisterBusinessTest {
+
+    @Test
+    @DisplayName("Test Register Failure , result = First name is required.")
+    public void case01(){
+        RegisterBusiness business = new RegisterBusiness();
+        Exception exception = assertThrows(ArgumentNullException.class, () -> {
+            business.register(null,new Speaker());
+        });
+        assertEquals("First name is required.",exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test Register Failure , result = Last name is required.")
+    public void case02(){
+        RegisterBusiness business = new RegisterBusiness();
+        Exception exception = assertThrows(ArgumentNullException.class, () -> {
+            Speaker speaker = new Speaker();
+            speaker.setFirstName("Arsom");
+            business.register(null,speaker);
+        });
+        assertEquals("Last name is required.",exception.getMessage());
+    }
+}
