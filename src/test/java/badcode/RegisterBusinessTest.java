ae512b4db649e3137d1191d7b8c10d48ff0180f7
Arsom Mahatthanamongkol <arsom.ma@cdg.co.th>
2021-10-01 10:01:23 +0700

    Test all

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -41,4 +41,140 @@ public class RegisterBusinessTest {
         });
         assertEquals("Email is required.",exception.getMessage());
     }
+
+    @Test
+    @DisplayName("Test Register Failure , result = DomainEmailInvalidException")
+    public void case04(){
+        RegisterBusiness business = new RegisterBusiness();
+        Exception exception = assertThrows(DomainEmailInvalidException.class, () -> {
+            Speaker speaker = new Speaker();
+            speaker.setFirstName("Arsom");
+            speaker.setLastName("Mahatthanamongkol");
+            speaker.setEmail("arsom");
+            business.register(null,speaker);
+        });
+    }
+
+    @Test
+    @DisplayName("Test Register Failure , result = Can't save a speaker.")
+    public void case05(){
+        RegisterBusiness business = new RegisterBusiness();
+        Exception exception = assertThrows(SaveSpeakerException.class, () -> {
+            Speaker speaker = new Speaker();
+            speaker.setFirstName("Arsom");
+            speaker.setLastName("Mahatthanamongkol");
+            speaker.setEmail("arsomm@gmail.com");
+            business.register(null,speaker);
+        });
+        assertEquals("Can't save a speaker.",exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test Register Failure,email not match domain , result = Speaker doesn't meet our standard rules.")
+    public void case06(){
+        RegisterBusiness business = new RegisterBusiness();
+        Exception exception = assertThrows(SpeakerDoesntMeetRequirementsException.class, () -> {
+            Speaker speaker = new Speaker();
+            speaker.setFirstName("Arsom");
+            speaker.setLastName("Mahatthanamongkol");
+            speaker.setEmail("arsomm@hotmail.com");
+            business.register(null,speaker);
+        });
+        assertEquals("Speaker doesn't meet our standard rules.",exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test Register ,exp 1 , result 1")
+    public void case07(){
+        SpeakerRepository repository  = new SpeakerRepository() {
+            @Override
+            public Integer saveSpeaker(Speaker speaker) {
+                return 1;
+            }
+        };
+        RegisterBusiness business = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Arsom");
+        speaker.setLastName("Mahatthanamongkol");
+        speaker.setEmail("arsomm@gmail.com");
+        speaker.setExp(1);
+        Integer result = business.register(repository,speaker);
+        assertEquals(1,result);
+    }
+
+    @Test
+    @DisplayName("Test Register ,exp 2 , result = 1")
+    public void case08(){
+        SpeakerRepository repository  = new SpeakerRepository() {
+            @Override
+            public Integer saveSpeaker(Speaker speaker) {
+                return 1;
+            }
+        };
+        RegisterBusiness business = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Arsom");
+        speaker.setLastName("Mahatthanamongkol");
+        speaker.setEmail("arsomm@gmail.com");
+        speaker.setExp(2);
+        Integer result = business.register(repository,speaker);
+        assertEquals(1,result);
+    }
+
+    @Test
+    @DisplayName("Test Register ,exp 3 , result = 1")
+    public void case09(){
+        SpeakerRepository repository  = new SpeakerRepository() {
+            @Override
+            public Integer saveSpeaker(Speaker speaker) {
+                return 1;
+            }
+        };
+        RegisterBusiness business = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Arsom");
+        speaker.setLastName("Mahatthanamongkol");
+        speaker.setEmail("arsomm@gmail.com");
+        speaker.setExp(3);
+        Integer result = business.register(repository,speaker);
+        assertEquals(1,result);
+    }
+
+    @Test
+    @DisplayName("Test Register ,exp 5 , result = 1")
+    public void case10(){
+        SpeakerRepository repository  = new SpeakerRepository() {
+            @Override
+            public Integer saveSpeaker(Speaker speaker) {
+                return 1;
+            }
+        };
+        RegisterBusiness business = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Arsom");
+        speaker.setLastName("Mahatthanamongkol");
+        speaker.setEmail("arsomm@gmail.com");
+        speaker.setExp(5);
+        Integer result = business.register(repository,speaker);
+        assertEquals(1,result);
+    }
+
+    @Test
+    @DisplayName("Test Register ,exp 9 , result = 1")
+    public void case11(){
+        SpeakerRepository repository  = new SpeakerRepository() {
+            @Override
+            public Integer saveSpeaker(Speaker speaker) {
+                return 1;
+            }
+        };
+        RegisterBusiness business = new RegisterBusiness();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Arsom");
+        speaker.setLastName("Mahatthanamongkol");
+        speaker.setEmail("arsomm@gmail.com");
+        speaker.setExp(9);
+        Integer result = business.register(repository,speaker);
+        assertEquals(1,result);
+    }
 }
