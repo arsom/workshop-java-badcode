6482fb5235bb5331cfb43b76adc34027ab2de0c1
Arsom Mahatthanamongkol <arsom.ma@cdg.co.th>
2021-10-01 10:36:31 +0700

    free test

---
+++ src/test/java/badcode/RegisterBusinessGetFreeTest.java
@@ -0,0 +1,25 @@
+package badcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RegisterBusinessGetFreeTest {
+    @ParameterizedTest
+    @CsvSource({
+            "0,500",
+            "1,500",
+            "2,250",
+            "3,250",
+            "4,100",
+            "5,100",
+            "8,50",
+            "9,50",
+            "10,0",
+    })
+    public void getFree(int exp,int free){
+        RegisterBusiness business = new RegisterBusiness();
+        int actualFree = business.getFee(exp);
+        Assertions.assertEquals(free,actualFree);
+    }
+}
